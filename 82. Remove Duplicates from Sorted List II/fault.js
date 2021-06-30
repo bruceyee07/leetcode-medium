376da7a4baf3e99e91522e12a4ac33c9a78048ac
bruceyee07 <yehao402@163.com>
2021-06-30 23:54:56 +0800

    remove-duplicates-from-sorted-list-ii wrong solution

---
+++ 82. Remove Duplicates from Sorted List II/fault.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @description https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+
+  let dummy = new ListNode(-1);
+  dummy.next = head;
+  
+  let node = dummy;
+  let map = {};
+
+  while (head) {
+    if (map[head.val]) {
+      map[head.val]++;
+    } else {
+      map[head.val] = 1;
+    }
+    head = head.next;
+  }
+
+  while (node.next) {
+    if (map[node.next.val] > 1) {
+      node = node.next.next;
+    } else {
+      node = node.next;
+    }
+  }
+
+  return dummy.next;
+};
+
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val);
+  this.next = (next === undefined ? null : next);
+}
