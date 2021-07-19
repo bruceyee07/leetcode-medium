736172974e24326dc28232a6afb35499f086b90d
bruceyee07 <yehao402@163.com>
2021-07-19 20:14:46 +0800

    sum-of-nodes-with-even-valued-grandparent recursive java solution

---
+++ 1315. Sum of Nodes with Even-Valued Grandparent/other.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/** 
+ * https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/
+ * https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/discuss/477095/Easy-DFS-solution
+ */
+class Solution {
+  public static int sum = 0;
+
+  public int sumEvenGrandparent(TreeNode root) {
+    dfs(root, null, null);
+    return sum;
+  }
+
+  void dfs(TreeNode current, TreeNode parent, TreeNode grandParent) {
+    if (current == null) return;
+    if (grandParent != null && grandParent.val % 2 == 0) {
+      sum += current.val;
+    }
+    dfs(current.left, current, parent);
+    dfs(current.right, current, parent);
+  }
+}
+
+/**
+ * I'm confused that why told me it's a wrong answer when submitted, whereas the test case passed
+ * test case: [50,null,54,98,6,null,null,null,34]
+ * expected: 138
+ * output: 156
+ */
