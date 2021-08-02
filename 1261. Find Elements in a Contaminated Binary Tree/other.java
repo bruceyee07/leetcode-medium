7198081512c2a3b0aa5896979a328b166e5875dc
bruceyee07 <yehao402@163.com>
2021-08-02 13:12:17 +0800

    find-elements-in-a-contaminated-binary-tree java solution using hashset

---
+++ 1261. Find Elements in a Contaminated Binary Tree/other.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/
+ * https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/discuss/431107/JavaPython-3-DFS-and-BFS-clean-codes-w-analysis.
+ */
+class FindElements {
+  private Set<Integer> seen = new HashSet<>();
+    
+  public FindElements(TreeNode root) {
+    dfs(root, 0);
+  }
+  
+  private void dfs(TreeNode n, int v) {
+    if (n == null) return;
+    seen.add(v);
+    n.val = v;
+    dfs(n.left, 2 * v + 1);
+    dfs(n.right, 2 * v + 2);
+  }
+
+  public boolean find(int target) {
+    return seen.contains(target);
+  }
+}
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements obj = new FindElements(root);
+ * boolean param_1 = obj.find(target);
+ */
