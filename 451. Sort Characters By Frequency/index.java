181f497b3e19ce457c011aff30e9edeba8ac8ce4
bruceyee07 <yehao402@163.com>
2021-09-12 14:05:33 +0800

    sort-characters-by-frequency using priorityqueue and stringbuilder

---
+++ 451. Sort Characters By Frequency/index.java
@@ -0,0 +1,9 @@
+/**
+ * https://leetcode.com/problems/sort-characters-by-frequency/
+ * self
+ */
+class Solution {
+  public String frequencySort(String s) {
+    
+  }
+}
---
+++ 451. Sort Characters By Frequency/other.java
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/sort-characters-by-frequency/
+ * https://leetcode.com/problems/sort-characters-by-frequency/discuss/93420/Java-O(n)-Bucket-Sort-Solution-O(nlogm)-PriorityQueue-Solution-easy-to-understand
+ */
+
+class Solution {
+  public String frequencySort(String s) {
+    HashMap<Character, Integer> map = new HashMap<>();
+    for (char c : s.toCharArray()) {
+      map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+
+    PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+    pq.addAll(map.entrySet());
+
+    StringBuilder sb = new StringBuilder();
+    while (!pq.isEmpty()) {
+      Map.Entry entry = pq.poll();
+      for (int i = 0; i < (int)entry.getValue(); i++)
+        sb.append(entry.getKey()); 
+    }
+    return sb.toString(); 
+  }
+}
