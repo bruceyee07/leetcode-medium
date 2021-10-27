bed88daf0cdb895a78ae869a510f6747e86dc368
bruceyee07 <yehao402@163.com>
2021-10-27 13:44:53 +0800

    lowest-common-ancestor-of-deepest-leaves wrong java solution

---
+++ 1123. Lowest Common Ancestor of Deepest Leaves/fault.java
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+ * self
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+ * self
+ */
+class Solution {
+  public TreeNode lcaDeepestLeaves(TreeNode root) {
+    HashMap<TreeNode, Integer> map = new HashMap<>();
+    TreeNode node = root;
+    int depth = 0;
+    map.put(node, depth);
+    if (node.left != null) {
+      map.put(node.left, depth++);
+      node = node.left;
+    }
+    if (node.right != null) {
+      map.put(node.right, depth++);
+      node = node.right;
+    }
+    return node;
+  }
+}
