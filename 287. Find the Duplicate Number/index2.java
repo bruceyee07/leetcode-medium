cdc1e9bfa5fb40ace45316de1bf2533fc9b003df
bruceyee07 <yehao402@163.com>
2021-11-01 23:50:29 +0800

    find-the-duplicate-number

---
+++ 287. Find the Duplicate Number/index2.java
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/find-the-duplicate-number/
+ * self
+ */
+class Solution {
+  public int findDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (int val : nums) {
+      if (set.contains(val)) return val;
+      set.add(val);
+    }
+    return -1;
+  }
+}
