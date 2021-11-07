f123ba6e9f6f7bd2d1efcde5d66c3e0b800d1a83
bruceyee07 <yehao402@163.com>
2021-11-07 15:35:58 +0800

    rotate-array

---
+++ 189. Rotate Array/other.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/rotate-array/
+ * https://leetcode.com/problems/rotate-array/discuss/54250/Easy-to-read-Java-solution
+ */
+class Solution {
+  public void rotate(int[] nums, int k) {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+  }
+
+  private void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+      int temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+  }
+}
