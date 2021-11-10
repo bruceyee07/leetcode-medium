55365a979348df20c4c70406f17cea483288ec39
bruceyee07 <yehao402@163.com>
2021-11-10 14:00:38 +0800

    rotate-image

---
+++ 48. Rotate Image/index.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/rotate-image/
+ * self
+ */
+class Solution {
+  public void rotate(int[][] matrix) {
+    int n = matrix.length;
+    int[][] temp = new int[n][n];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        temp[i][j] = matrix[n - 1 - j][i];
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        matrix[i][j] = temp[i][j];
+      }
+    }
+  }
+}
